2218cfeabeb21f465d369401d678daf93ab196d0
Shafeequemk80 <64883556+Shafeequemk80@users.noreply.github.com>
2024-10-31 03:58:28 +0530

    change vite config again

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,32 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-// Define backend URLs for different environments
-const backendUrl = process.env.NODE_ENV === 'production'
-  ? 'https://cholayilmadrassa-be.onrender.com'
-  : 'http://localhost:8000';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5000,
-    proxy: {
-      '/api': {
-        target: backendUrl,
-        changeOrigin: true,
-        secure: process.env.NODE_ENV === 'production', // Only secure in production
-      },
-    },
-  },
-});
+})
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // Define backend URLs for different environments
+// const backendUrl = process.env.NODE_ENV === 'production'
+//   ? 'https://cholayilmadrassa-be.onrender.com'
+//   : 'http://localhost:8000';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0',
+//     port: 5000,
+//     proxy: {
+//       '/api': {
+//         target: backendUrl,
+//         changeOrigin: true,
+//         secure: process.env.NODE_ENV === 'production', // Only secure in production
+//       },
+//     },
+//   },
+// });
